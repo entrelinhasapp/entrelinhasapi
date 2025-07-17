5872cbd046cb3e88df2424d122491a60944f8e9f
entrelinhasapp <entrelinhasapp1@gmail.com>
2025-07-17 14:50:32 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+import pandas as pd
+import numpy as np
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return JSONResponse(content={"message": "API está online!"})
+
+
+# Rota dinâmica (GET)
+@app.get("/livros/{id_livro}/")
+def dados_livro(id_livro: int):
+    #df = pd.read_csv('https://raw.githubusercontent.com/diegodio/entrelinhasapi/refs/heads/main/db_livros.csv?token=GHSAT0AAAAAADHNQBSHZNQX7EPA2VWPL5HE2DZF2FQ')
+    df = pd.read_csv('db_livros.csv', encoding='utf-8')
+
+    df = df[df['ID livro'] == id_livro]
+
+
+    #return JSONResponse(content=dic_info)
+    return JSONResponse(content=df.to_dict(orient="records")[0])
+
+
